ab45885e5ffa5940b590c54a6bcc891ff4d0a017
Deden Fathurahman <kompiebutut@gmail.com>
2013-01-31 02:16:01 +0700

    Import opscode-cookbooks/rabbitmq version d1a547acea072f7cc345e6a86a4f78ee5b3f6f1f

---
+++ chef/cookbooks/rabbitmq/providers/user.rb
@@ -0,0 +1,119 @@
+#
+# Cookbook Name:: rabbitmq
+# Provider:: user
+#
+# Copyright 2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+def user_exists?(name)
+  cmd = Mixlib::ShellOut.new("rabbitmqctl list_users |grep '^#{name}\\b'")
+  cmd.environment['HOME'] = ENV.fetch('HOME', '/root')
+  cmd.run_command
+  begin
+    cmd.error!
+    true
+  rescue
+    false
+  end
+end
+
+def user_has_tag?(name, tag)
+  tag = '"\[\]"' if tag.nil?
+  cmdStr = "rabbitmqctl list_users | grep \"^#{name}\\b\" | grep #{tag}"
+  cmd = Mixlib::ShellOut.new(cmdStr)
+  cmd.environment['HOME'] = ENV.fetch('HOME', '/root')
+  cmd.run_command
+  begin
+    cmd.error!
+    true
+  rescue Exception => e
+    false
+  end
+end
+
+def user_has_rights?(vhost,name,perm_list)
+  if vhost.nil?
+    cmdStr = "rabbitmqctl list_permissions | grep \"^#{name}\\b\""
+  else
+    cmdStr = "rabbitmqctl list_permissions -p  #{vhost} | grep \"^#{name}\\b\""
+  end
+  cmd = Mixlib::ShellOut.new(cmdStr)
+  cmd.environment['HOME'] = ENV.fetch('HOME', '/root')
+  cmd.run_command
+  begin
+    cmd.error!
+    current_permissions = cmd.stdout.each_line.first.split.drop(1)
+    Chef::Log.info "Rights for #{name} are #{current_permissions} (we want #{perm_list})"
+    current_permissions == perm_list
+  rescue Exception => e
+    Chef::Log.warn "User has probably no right on that virtual host: #{e}"
+    false
+  end
+end
+
+
+action :add do
+  unless user_exists?(new_resource.user)
+    execute "rabbitmqctl add_user #{new_resource.user} #{new_resource.password}" do
+      Chef::Log.info "Adding RabbitMQ user '#{new_resource.user}'."
+      new_resource.updated_by_last_action(true)
+    end
+  end
+end
+
+action :delete do
+  if user_exists?(new_resource.user)
+  execute "rabbitmqctl delete_user #{new_resource.user}" do
+    Chef::Log.info "Deleting RabbitMQ user '#{new_resource.user}'."
+    new_resource.updated_by_last_action(true)
+  end
+end
+end
+
+action :set_permissions do
+  perm_list = new_resource.permissions.split
+  unless user_has_rights?(new_resource.vhost, new_resource.user, perm_list)
+    vhostOpt = "-p #{new_resource.vhost}" unless new_resource.vhost.nil?
+    execute "rabbitmqctl set_permissions #{vhostOpt} #{new_resource.user} \"#{perm_list.join("\" \"")}\"" do
+      Chef::Log.info "Setting RabbitMQ user permissions for '#{new_resource.user}' on vhost #{new_resource.vhost}."
+      new_resource.updated_by_last_action(true)
+    end
+  end
+end
+
+action :clear_permissions do
+  if new_resource.vhost
+    execute "rabbitmqctl clear_permissions -p #{new_resource.vhost} #{new_resource.user}" do
+      only_if "rabbitmqctl list_user_permissions | grep #{new_resource.user}"
+      Chef::Log.info "Clearing RabbitMQ user permissions for '#{new_resource.user}' from vhost #{new_resource.vhost}."
+      new_resource.updated_by_last_action(true)
+    end
+  else
+    execute "rabbitmqctl clear_permissions #{new_resource.user}" do
+      only_if "rabbitmqctl list_user_permissions | grep #{new_resource.user}"
+      Chef::Log.info "Clearing RabbitMQ user permissions for '#{new_resource.user}'."
+      new_resource.updated_by_last_action(true)
+    end
+  end
+end
+
+action :set_user_tags do
+  unless user_has_tag?(new_resource.user, new_resource.user_tag)
+    execute "rabbitmqctl set_user_tags #{new_resource.user} #{new_resource.user_tag}" do
+      Chef::Log.info "Setting RabbitMQ user tag '#{new_resource.user_tag}' on '#{new_resource.user}'"
+      new_resource.updated_by_last_action(true)
+    end
+  end
+end
